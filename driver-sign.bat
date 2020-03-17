af7ebb79d9be49a35cce0bf5b2ae1e129506fec8
Toni Uhlig <matzeton@googlemail.com>
2020-03-17 21:04:06 +0100

    * driver sign script signs release as well * removed annoying cert passphrase
    Signed-off-by: Toni Uhlig <matzeton@googlemail.com>

--- driver-sign.bat
+++ driver-sign.bat
@@ -1,7 +1,8 @@
 @echo off
 
 set SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe"
-set DRIVER="%~dp0\x64\Debug\PastDSEDriver.sys"
+set DRIVER_DEBUG="%~dp0x64\Debug\PastDSEDriver.sys"
+set DRIVER_RELEASE="%~dp0x64\Release\PastDSEDriver.sys"
 set CA="%~dp0\VeriSign Class 3 Public Primary Certification Authority - G5.cer"
 set CERT="%~dp0\cert.pfx"
 
@@ -14,14 +15,15 @@ echo driver..: %DRIVER%
 echo ca......: %CA%
 echo cert....: %CERT%
 echo ---------------------------
-%SIGNTOOL% sign /a /ac %CA% /f %CERT% /p "1" %DRIVER%
+%SIGNTOOL% sign /a /ac %CA% /f %CERT% %DRIVER_DEBUG%
+%SIGNTOOL% sign /a /ac %CA% /f %CERT% %DRIVER_RELEASE%
 echo ***************************
-%SIGNTOOL% verify /kp /v %DRIVER%
+%SIGNTOOL% verify /kp /v %DRIVER_DEBUG%
+%SIGNTOOL% verify /kp /v %DRIVER_RELEASE%
 echo ***************************
 
 net stop w32time
 net start w32time
 w32tm /resync /nowait
 
-REM pause
-timeout /t 3
+pause
