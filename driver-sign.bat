bb7aa982af521a79459b48f9a6c702b0e6af3cb8
segfault <toni@impl.cc>
2020-05-22 19:10:29 +0200

    VS2019 support

--- driver-sign.bat
+++ driver-sign.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe"
+set SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool.exe"
 set DRIVER_DEBUG="%~dp0x64\Debug\PastDSEDriver.sys"
 set DRIVER_RELEASE="%~dp0x64\Release\PastDSEDriver.sys"
 set CA="%~dp0\certs\VeriSign Class 3 Public Primary Certification Authority - G5.cer"
